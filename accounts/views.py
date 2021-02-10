e70263bd6dd653809a33edbe936c50fdbe86246c
SauravJalui <saurav.jalui-codes@outlook.com>
2021-02-10 18:13:24 +0530

    updated top level import in views

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.views import generic
+from django.views.generic import CreateView
 from django.urls import reverse_lazy
 
-class SignUpView(generic.CreateView):
+class SignUpView(CreateView):
     '''This is the signup view using built-in UserCreationForm'''
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
